fecfca3819ac18e936749671281ef7ceefbc8a46
Joshua Holland <joshua.holland.1@outlook.com>
2024-02-25 21:55:39 -0500

    created service layer test classes and added application.yml in test environment

---
+++ src/test/java/alpha/com/starmobile/services/PlanServiceTests.java
@@ -0,0 +1,27 @@
+package alpha.com.starmobile.services;
+
+import org.junit.jupiter.api.Test;
+
+public class PlanServiceTests {
+
+    @Test
+    void testSave() {
+        // TODO
+    }
+
+    @Test
+    void testFindAll() {
+        // TODO
+    }
+
+    @Test
+    void testfindById() {
+        // TODO
+    }
+
+    @Test
+    void testFindAllByPlanType() {
+        // TODO
+    }
+
+}
