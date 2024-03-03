8abaa55fec2df1a602a5a2bc91b2240ff8ff4375
Joshua Holland <joshua.holland.1@outlook.com>
2024-03-03 13:27:09 -0500

    established base structure for page

--- application-frontend/src/app/app.component.ts
+++ application-frontend/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { FooterBarComponent } from './footer-bar/footer-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuBarComponent, SideBarComponent, MainContentComponent, FooterBarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular';
