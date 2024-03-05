3eed821034b72b18ad5d5201ff3d67324aed0e97
Hunter Halstead <hunterhalstead@gmail.com>
2024-03-05 18:15:43 -0500

    plan-card

---
+++ application-frontend/src/app/plan-card/plan-card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-plan-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './plan-card.component.html',
+  styleUrl: './plan-card.component.css'
+})
+export class PlanCardComponent {
+
+}
