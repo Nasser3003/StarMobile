59936aa319ede840a330e6fed8af018ab609053b
Joshua Holland <joshua.holland.1@outlook.com>
2024-03-05 13:56:07 -0500

    new header and footer, created models and services

--- application-frontend/src/app/app.component.ts
+++ application-frontend/src/app/app.component.ts
@@ -3,13 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { MenuBarComponent } from './menu-bar/menu-bar.component';
 import { MainContentComponent } from './main-content/main-content.component';
 import { FooterBarComponent } from './footer-bar/footer-bar.component';
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, MenuBarComponent, MainContentComponent, FooterBarComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, MenuBarComponent, MainContentComponent, FooterBarComponent, HeaderComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'angular';
