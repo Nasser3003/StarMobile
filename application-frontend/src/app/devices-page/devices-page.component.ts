165519b50dbc2532a55da75ef805cf193f3b3050
Joshua Holland <joshua.holland.1@outlook.com>
2024-03-05 19:49:26 -0500

    created device card

--- application-frontend/src/app/devices-page/devices-page.component.ts
+++ application-frontend/src/app/devices-page/devices-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { DeviceCardComponent } from "../device-card/device-card.component";
 
 @Component({
-  selector: 'app-devices-page',
-  standalone: true,
-  imports: [],
-  templateUrl: './devices-page.component.html',
-  styleUrl: './devices-page.component.css'
+    selector: 'app-devices-page',
+    standalone: true,
+    templateUrl: './devices-page.component.html',
+    styleUrl: './devices-page.component.css',
+    imports: [DeviceCardComponent]
 })
 export class DevicesPageComponent {
 
