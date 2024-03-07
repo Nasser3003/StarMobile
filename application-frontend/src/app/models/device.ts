6914d70525c139f7a1c57102d83a4cced55e8ff1
Joshua Holland <joshua.holland.1@outlook.com>
2024-03-06 20:47:45 -0500

    added properties and constructors to front end models

--- application-frontend/src/app/models/device.ts
+++ application-frontend/src/app/models/device.ts
@@ -1,2 +1,23 @@
 export class Device {
+
+    brand: string;
+    description: string;
+    id: number;
+    model: string;
+    price: number;
+
+    constructor(
+        brand: string,
+        description: string,
+        id: number,
+        model: string,
+        price: number
+    ) {
+        this.brand = brand;
+        this.description = description;
+        this.id = id;
+        this.model = model;
+        this.price = price;
+    }
+
 }
--- application-frontend/src/app/models/line.ts
+++ application-frontend/src/app/models/line.ts
@@ -1,2 +1,11 @@
 export class Line {
+    deviceID: number;
+    id: number;
+    number: string;
+
+    constructor(deviceID: number, id: number, number: string) {
+        this.deviceID = deviceID;
+        this.id = id;
+        this.number = number;
+    }
 }
