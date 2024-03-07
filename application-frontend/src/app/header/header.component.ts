d81f7c69195020e281934af5c5697eb29bb4ada0
Joshua Holland <joshua.holland.1@outlook.com>
2024-03-06 22:42:09 -0500

    got routes for all main pages working and added some styling to the header component

--- application-frontend/src/app/header/header.component.ts
+++ application-frontend/src/app/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLinkActive, RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
