14ed071303fd5bdaa25bb264bd73b7595ad2170c
Joshua Holland <joshua.holland.1@outlook.com>
2024-03-08 15:27:35 -0500

    added auth service

--- application-frontend/src/app/login/login.component.ts
+++ application-frontend/src/app/login/login.component.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
+import { Login } from '../interfaces/login';
 
 @Component({
   selector: 'app-login',
@@ -13,11 +16,16 @@ export class LoginComponent {
 
   loginForm: FormGroup;
 
-  constructor(private formBuilder: FormBuilder) {
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) {
     this.loginForm = this.formBuilder.group({
       loginEmail: ['', Validators.compose([Validators.required, Validators.minLength(5), Validators.email])],
       loginPassword: ['', Validators.compose([Validators.required, Validators.minLength(8), Validators.maxLength(25)])]
     });
+    if (authService.isLoggedIn) {
+      const redirectUrl = this.authService.accountRedirectUrl
+      ? this.authService.accountRedirectUrl: '/account';
+      this.router.navigate([redirectUrl]);
+    }
   }
 
   get loginEmail() {
@@ -30,6 +38,13 @@ export class LoginComponent {
   login() {
     // login logic here, probably call backend
     if(this.loginForm.valid) {
+      this.authService.login().subscribe(() => {
+        if (this.authService.isLoggedIn) {
+          const redirectUrl = this.authService.accountRedirectUrl
+          ? this.authService.accountRedirectUrl: '/account';
+          this.router.navigate([redirectUrl])
+        }
+      })
       console.log('Login sent!', this.loginForm.value);
     }
   }
