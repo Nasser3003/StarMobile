a32268cb6e0617aff9d99ec5caf81d73b6bdc1fc
palmerrodw <palmerrodw@yahoo.com>
2024-03-08 09:01:32 -0500

    UserController update

--- src/main/java/alpha/com/starmobile/StarMobile.java
+++ src/main/java/alpha/com/starmobile/StarMobile.java
@@ -2,8 +2,10 @@ package alpha.com.starmobile;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"services", "alpha.com.starmobile.repository"})
 public class StarMobile {
 
 	public static void main(String[] args) {
