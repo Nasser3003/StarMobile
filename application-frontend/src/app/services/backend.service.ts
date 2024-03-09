1b3373a1e147da612c0fb4c2c0293bbedac5d803
Joshua Holland <joshua.holland.1@outlook.com>
2024-03-09 13:14:32 -0500

    initial code for line API calls

--- application-frontend/src/app/services/backend.service.ts
+++ application-frontend/src/app/services/backend.service.ts
@@ -3,6 +3,8 @@ import { environment } from '../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { User } from '../models/user';
 import { Device } from '../models/device';
+import { Line } from '../models/line';
+import { Plan } from '../models/plan';
 
 @Injectable({
   providedIn: 'root'
@@ -10,10 +12,31 @@ import { Device } from '../models/device';
 export class BackendService {
 
   baseURL = environment.apiURL;
+
   postUser = new User(0,'','','','');
   getUser = new User(0,'','','','');
-  updatedUser = new User(0,'','','','');
+  putUser = new User(0,'','','','');
   deletedUser = new User(0,'','','','');
+  allUsers: User[] = [];
+
+  postDevice = new Device('','',0,'',0);
+  getDevice = new Device('','',0,'',0);
+  putDevice = new Device('','',0,'',0);
+  deletedDevice = new Device('','',0,'',0);
+  allDevices: Device[] = [];
+
+  postLine = new Line(0,0,'');
+  getLine = new Line(0,0,'');
+  putLine = new Line(0,0,'');
+  deletedLine = new Line(0,0,'');
+  allLines: Line[] = [];
+
+  postPlan = new Plan(0,0,0,0,'');
+  getPlan = new Plan(0,0,0,0,'');
+  putPlan = new Plan(0,0,0,0,'');
+  deletedPlan = new Plan(0,0,0,0,'');
+  allPlans: Plan[] = [];
+
 
   constructor(private http: HttpClient) { }
 
@@ -66,7 +89,7 @@ export class BackendService {
    */
   getAllUsers() {
     this.http.get<any>(this.baseURL + 'user', {observe: 'response'}).subscribe({
-      next : data => this.getUser = data.body.data,
+      next : data => this.allUsers = data.body.data,
       error: err => console.log(err),
       complete: () => console.log('All users retrieved')
     });
@@ -77,7 +100,7 @@ export class BackendService {
    */
   editUser(id: string, user: {}) {
     this.http.put<any>(this.baseURL + 'user/' + id, user, {observe: 'response'}).subscribe({
-      next : data => this.getUser = data.body.data,
+      next : data => this.putUser = data.body.data,
       error: err => console.log(err),
       complete: () => console.log('User edited')
     });
@@ -101,7 +124,7 @@ export class BackendService {
    */
   getAllDevices() {
     this.http.get<any>(this.baseURL + 'device', {observe: 'response'}).subscribe({
-      next : data => this.getUser = data.body.data,
+      next : data => this.allDevices = data.body.data,
       error: err => console.log(err),
       complete: () => console.log('All devices retrieved')
     });
@@ -112,7 +135,7 @@ export class BackendService {
    */
   getDeviceById(id: string) {
     this.http.get<any>(this.baseURL + 'device/' + id, {observe: 'response'}).subscribe({
-      next : data => this.getUser = data.body.data,
+      next : data => this.getDevice = data.body.data,
       error: err => console.log(err),
       complete: () => console.log('Device retrieved by ID')
     });
@@ -123,7 +146,7 @@ export class BackendService {
    */
   createDevice(device: Device) {
     this.http.post<any>(this.baseURL + 'device', {observe: 'response'}).subscribe({
-      next : data => this.getUser = data.body.data,
+      next : data => this.postDevice = data.body.data,
       error: err => console.log(err),
       complete: () => console.log('Device created')
     });
@@ -134,9 +157,9 @@ export class BackendService {
    */
   updateDevice(id: string) {
     this.http.put<any>(this.baseURL + 'device/' + id, {observe: 'response'}).subscribe({
-      next : data => this.getUser = data.body.data,
+      next : data => this.putDevice = data.body.data,
       error: err => console.log(err),
-      complete: () => console.log('All devices retrieved')
+      complete: () => console.log('Device updated')
     });
   }
 
@@ -145,9 +168,9 @@ export class BackendService {
    */
   deleteDevice(id: string) {
     this.http.delete<any>(this.baseURL + 'device/' + id, {observe: 'response'}).subscribe({
-      next : data => this.getUser = data.body.data,
+      next : data => this.deletedDevice = data.body.data,
       error: err => console.log(err),
-      complete: () => console.log('All devices retrieved')
+      complete: () => console.log('Device deleted')
     });
   }
 
@@ -159,42 +182,66 @@ export class BackendService {
    * GET /line
    */
   getAllLines() {
-
+    this.http.get<any>(this.baseURL + 'line', {observe: 'response'}).subscribe({
+      next : data => this.allLines = data.body.data,
+      error: err => console.log(err),
+      complete: () => console.log('All lines retrieved')
+    });
   }
 
   /**
    * GET /line/{id}
    */
-  getLineById() {
-
+  getLineById(id: string) {
+    this.http.get<any>(this.baseURL + 'line' + id, {observe: 'response'}).subscribe({
+      next : data => this.getLine = data.body.data,
+      error: err => console.log(err),
+      complete: () => console.log('Line retrieved by ID')
+    });
   }
 
   /**
    * GET /line/{number}
    */
-  getLineByNumber() {
-
+  getLineByNumber(num: number) {
+    this.http.get<any>(this.baseURL + 'line' + num, {observe: 'response'}).subscribe({
+      next : data => this.getLine = data.body.data,
+      error: err => console.log(err),
+      complete: () => console.log('Line retrieved by phone number')
+    });
   }
 
   /**
    * POST /line
    */
   createLine() {
-
+    this.http.post<any>(this.baseURL + 'line', {observe: 'response'}).subscribe({
+      next : data => this.postLine = data.body.data,
+      error: err => console.log(err),
+      complete: () => console.log('Line created')
+    });
   }
 
   /**
    * PUT /line/{id}
    */
-  updateLine() {
-
+  updateLine(id: string) {
+    this.http.put<any>(this.baseURL + 'line/' + id, {observe: 'response'}).subscribe({
+      next : data => this.putLine = data.body.data,
+      error: err => console.log(err),
+      complete: () => console.log('Line updated')
+    });
   }
 
   /**
    * DELETE /line/{id}
    */
-  deleteLine() {
-
+  deleteLine(id: string) {
+    this.http.delete<any>(this.baseURL + 'line/' + id, {observe: 'response'}).subscribe({
+      next : data => this.deletedLine = data.body.data,
+      error: err => console.log(err),
+      complete: () => console.log('Line deleted')
+    });
   }
 
   ///////////////
