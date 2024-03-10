46aea11456b68588c6b72af641034e1fda09c8ce
Abdrabbo MBP 14 M1 <abdo.abdo3003@gmail.com>
2024-03-10 14:28:15 +0200

    Added custom login method

--- src/main/java/alpha/com/starmobile/controllers/AuthController.java
+++ src/main/java/alpha/com/starmobile/controllers/AuthController.java
@@ -1,13 +1,17 @@
 package alpha.com.starmobile.controllers;
 
+import alpha.com.starmobile.dto.LoginDTO;
 import alpha.com.starmobile.dto.RegistrationDTO;
 import alpha.com.starmobile.models.User;
+import alpha.com.starmobile.services.AuthenticationService;
 import alpha.com.starmobile.services.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -16,10 +20,17 @@ import org.springframework.web.bind.annotation.*;
 public class AuthController {
 
     private UserService service;
+    private AuthenticationService authService;
 
     @PostMapping("/register")
     public ResponseEntity<User> register(@RequestBody RegistrationDTO registrationDTO) {
         return new ResponseEntity<>(service.register(registrationDTO), HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<User> login(@RequestBody LoginDTO loginDTO) {
+        Optional<User> authenticatedUser = authService.authenticate(loginDTO);
+        return authenticatedUser.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
     }
 
 }
