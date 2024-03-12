b0b4188e0548123bd7d1301e33fa64a4d08ae5dc
Hunter Halstead <hunterhalstead@gmail.com>
2024-03-12 19:29:39 -0400

    tiny ngif change

--- application-frontend/src/app/plan-card/plan-card.component.ts
+++ application-frontend/src/app/plan-card/plan-card.component.ts
@@ -3,6 +3,7 @@ import { Component, inject } from '@angular/core';
 import { PhonePipe } from '../pipes/phone.pipe';
 import { AuthService } from '../services/auth.service';
 import { Plan } from '../models/plan';
+import { User } from '../models/user';
 
 @Component({
     selector: 'app-plan-card',
@@ -12,16 +13,29 @@ import { Plan } from '../models/plan';
     imports: [CommonModule, PhonePipe]
 })
 export class PlanCardComponent {
-  LoginStatus: AuthService = inject(AuthService);
+  // LoginStatus: AuthService = inject(AuthService);
 
   citPlan: Plan = new Plan("Citizen", 25, 150, "galactic")
   starPlan: Plan = new Plan("Starfighter", 20, 150, "universal")
   droidPlan: Plan = new Plan("Droid", 100, 10000, "galactic")
   larvaPlan: Plan = new Plan("Larval", 5, 10, "system")
+  // lineList: string[] = ['5096270952', '0983427547'] //Removable. was for HW, same for phonePipe above
+  currentUser: User | null = null;
+  isLoggedIn: boolean = false;
+
+  ngOnInit(): void {
+    this.auth.currentUser.subscribe(user => {
+      if(user !== null) {
+        this.currentUser = user;
+      }
+    });
+    this.auth.isLoggedIn.subscribe(isLoggedIn => this.isLoggedIn = isLoggedIn);
+  }
 
   listOfAvailablePlansDefault: Plan[] = [this.citPlan, this.starPlan, this.droidPlan, this.larvaPlan];
 
-  // lineList: string[] = ['5096270952', '0983427547'] //Removable. was for HW, same for phonePipe above
+  constructor(private auth: AuthService) {
 
+  }
 }
 
