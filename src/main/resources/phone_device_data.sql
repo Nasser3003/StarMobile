660179eab7a2450ad938700b21575a79cbc2bed4
palmerrodw <palmerrodw@yahoo.com>
2024-03-14 13:31:02 -0400

    Create phone_device_data.sql

---
+++ src/main/resources/phone_device_data.sql
@@ -0,0 +1,9 @@
+INSERT INTO device (brand, model, description, price, picture_path) VALUES
+("Ploklok", "Ashen Nightmare 3", "Starliner class phone from Ploklok.", 1300, '/assets/imgs/devices/ashen_nightmar_3.jfif'),
+("Ploklok", "Ashen Daydream 3", "Liner class phone from Ploklok.", 300, '/assets/imgs/devices/ashen_daydream_3.jfif'),
+("Quantum", "1x", "A singular phone from Quantum.", 1200, '/assets/imgs/devices/1x.jpg'),
+("Quantum", "1", "Another singular phone from Quantum.", 400, '/assets/imgs/devices/1.jpg'),
+("BlackHoleBerry", "3 pro", "BlackHoleBerry from beyond the horizon", 700, '/assets/imgs/devices/3_pro.jfif'),
+("BlackHoleBerry", "3 no", "BlackHoleBerry at the cutting edge.", 450, '/assets/imgs/devices/3_no.jfif'),
+("Hive", "Billion 8", "Say Hive to a billion.", 1300, '/assets/imgs/devices/billion_8.jfif'),
+("Hive", "Million 8", "Say Hive to a million.", 1300, '/assets/imgs/devices/million_8.jfif');
