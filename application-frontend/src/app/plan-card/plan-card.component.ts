ad96ca7137b353df0d7231f3757b155a33c136d5
Hunter Halstead <hunterhalstead@gmail.com>
2024-03-14 14:09:51 -0400

    plan page update - just plan method

--- application-frontend/src/app/plan-card/plan-card.component.ts
+++ application-frontend/src/app/plan-card/plan-card.component.ts
@@ -17,15 +17,25 @@ import { Line } from '../models/line';
 })
 export class PlanCardComponent {
   DefaultValues: BackendService = inject(BackendService);
-
+  
   // citPlan: Plan = new Plan("Citizen", 25, 150, "galactic")
   // starPlan: Plan = new Plan("Starfighter", 20, 150, "universal")
   // droidPlan: Plan = new Plan("Droid", 100, 10000, "galactic")
   // larvaPlan: Plan = new Plan("Larval", 5, 10, "system")
   // lineList: string[] = ['5096270952', '0983427547'] //Removable. was for HW, same for phonePipe above
-
+  
   currentUser: User = new User('', '', '', '');
   isLoggedIn: boolean = false;
+  
+  addJustPlan(chosenPlan: Plan) {
+    if(this.DefaultValues.allPlans.includes(chosenPlan)){
+      console.log("addJustPlan: Plan already found")
+    }
+    else {
+      this.DefaultValues.addPlan(chosenPlan.planType)
+      console.log("addJustPlan: Plan added to account")
+    }
+  }
 
   addPlanWithDevice(chosenPlan: Plan, chosenDevice: Device): void {
     if(this.DefaultValues.allPlans.includes(chosenPlan)){
