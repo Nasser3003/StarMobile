95859f47038ac1816ffff54a11c7621bf799f0bb
Abdrabbo MBP 14 M1 <abdo.abdo3003@gmail.com>
2024-03-14 23:15:26 +0200

    fixed remove device, decoupled service

--- src/main/java/alpha/com/starmobile/repository/DeviceRepository.java
+++ src/main/java/alpha/com/starmobile/repository/DeviceRepository.java
@@ -1,6 +1,7 @@
 package alpha.com.starmobile.repository;
 
 import alpha.com.starmobile.models.Device;
+import alpha.com.starmobile.models.Line;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,5 +11,5 @@ public interface DeviceRepository extends JpaRepository<Device, Long> {
     List<Device> findAllByBrand(String brand);
     List<Device> findAllByModel(String brand);
 
-    Optional<Device> findDeviceByBrandAndModel(String brand, String model);
+    Optional<Device> findDeviceByBrandAndModelAndLine(String brand, String model, Line line);
 }
